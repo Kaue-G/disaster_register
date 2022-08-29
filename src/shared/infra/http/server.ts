b0ec449b7b6251dc80f96771ce871dc6556a8270
Kaue-G <kaueguedes.dev@gmail.com>
2022-08-29 09:50:32 -0300

    end settings.

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,21 +3,17 @@ import 'reflect-metadata'
 import { AppDataSource } from '../typeorm'
 
 AppDataSource.initialize().then(() => {
-    console.log("on")
+    const app = express();
 
-    // const app = express();
+    app.use(express.json())
 
-    // app.use(express.json())
+    app.get('/', (request: Request,
+        response: Response,) => {
+        return response.json('tudo certo')
+    })
 
-    // app.get('/', (request: Request,
-    //     response: Response,) => {
-    //     return response.json('tudo certo')
-    // })
-    // console.log(process.env.SERVER_PORT);
-
-    // return app.listen(process.env.SERVER_PORT, () => console.log("server on"))
+    return app.listen(process.env.SERVER_PORT, () => console.log("server on"))
 })
 .catch(
-    // (err) => console.log(err)
     (err) => console.log(err)
 )
